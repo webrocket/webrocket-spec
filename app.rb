162ef4db1782af2bf2c1e49dd18d93a1d1cfffdc
Chris Kowalik <dev+nu7hatch@cuboxsa.com>
2011-12-13 19:02:51 -0200

    fixed app

--- app.rb
+++ app.rb
@@ -7,7 +7,3 @@ set :public_folder, File.join(settings.root, 'www')
 get '/' do
   "WebRocket Specification RFC"
 end
-
-get '/*.html' do |path|
-  File.read(File.join(settings.public_folder, path + ".html"))
-end
